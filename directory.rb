e632aaa6f8f46609b59670cd13f487c5337cef63
Maria Vanaale <mvanaale@gmail.com>
2019-01-28 21:32:33 +0000

    Third commit

--- directory.rb
+++ directory.rb
@@ -1,7 +1,7 @@
-#First we print the list of students
+student_count = 11
+#first, we print the list of students"
 puts "The students of Villains Academy"
 puts "-------------------------"
-
 puts "Dr. Hannibal Lecter"
 puts "Darth Vader"
 puts "Nurse Ratched"
@@ -14,9 +14,4 @@ puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
 # finally, we print the total number of students
-print "Overall, we have "
-#it's important that print() doesn't add new line characters
-print 11
-puts " great students"
-
-print "line1\nline2\nline3\n"
+puts "Overall, we have #{student_count} great students"
