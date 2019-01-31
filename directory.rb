26e4f6b19e61465748346e1c0c5baa37ac2da5df
Maria Vanaale <mvanaale@gmail.com>
2019-01-31 15:17:43 +0000

    last commit

--- directory.rb
+++ directory.rb
@@ -1,6 +1,38 @@
 #let's put all students into an array
 @students = [] #an emptu array accesible to all methods
 
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the students file to disk"
+  puts "4. Load the students file from disk"
+  puts "9. Exit"
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
+  end
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "4"
+    load_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -12,10 +44,16 @@ def input_students
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
     #get another name from the user
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
   #return the array of students
   #students
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
 end
 
 def print_header
@@ -29,37 +67,8 @@ def print_students_list
   end
 end
 
-def print_footer(names)
+def print_footer
   puts "Overall, we have #{@students.count} great students"
-end
-
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the students file to disk"
-  puts "4. Load the students file from disk"
-  puts "9. Exit"
-end
-
-def show_student
-  print_header
-  print_students_list
-  print_footer
-end
-
-def process(selection)
-  case selection
-  when "1"
-    students = input_students
-  when "2"
-    print_header
-    print(students)
-    print_footer(students)
-  when "9"
-    exit
-  else
-    puts "I don't know what you meant, try again"
-  end
 end
 
 def save_students
@@ -90,14 +99,6 @@ def try_load_students
   else #if it doesn't exist
     puts "Sorry, #{filename} doesn't exist."
     exit #quit the program
-  end
-end
-
-def interactive_menu
-  students = []
-  loop do
-    print_menu
-    process(STDIN.gets.chomp)
   end
 end
 
