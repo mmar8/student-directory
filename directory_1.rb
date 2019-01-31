de9342aadb957d4459902593c850ab3dc5651d59
Maria Vanaale <mvanaale@gmail.com>
2019-01-31 12:29:00 +0000

    new commit

--- directory_1.rb
+++ directory_1.rb
@@ -49,3 +49,35 @@ students = input_students
 print_header
 print(students)
 print_footer(students)
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the students file to disk"
+  puts "4. Load the students file from disk"
+  puts "9. Exit"
+end
+
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
+interactive_menu 
